cc83fd53c1eb0ffaed9909d6adc18a75b917ceb0
Sachin Goyal <sgoyal6@L-SB8M1HEG8W-M.local>
2016-09-14 20:44:47 -0700

    import cucumber.api.SnippetType;

--- tutorial-5/src/test/java/com/prismoskills/tutorials/RunCukesTest.java
+++ tutorial-5/src/test/java/com/prismoskills/tutorials/RunCukesTest.java
@@ -3,6 +3,7 @@ package com.prismoskills.tutorials;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
